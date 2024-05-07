b900f6dc194c4bd9539d93cc044fefddf07d06ab
JoshDeveloper25 <amandasarvelasquez@gmail.com>
2024-05-07 14:05:20 -0600

    Links to respective webpage links from Empresas

--- src/pages/Empresas/pages/AtlantidaInsurance/components/AtlantidaInsuranceInicio.jsx
+++ src/pages/Empresas/pages/AtlantidaInsurance/components/AtlantidaInsuranceInicio.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import casaValoresHeader from "../../../../../images/atlantida-insurance-inicio.png";
 import logoReditum from "../../../../../images/nuestraempresa7.png";
 
 const AtlantidaInsuranceInicio = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="bg-tertiary-color">
       <div className="container-page flex flex-col lg:flex-row justify-between items-center md:gap-3">
@@ -20,10 +23,7 @@ const AtlantidaInsuranceInicio = () => {
             mejores condiciones agregando valor tecnológico y operativo a
             nuestros asegurados.
           </p>
-          <button
-            className="button-red-primary"
-            type="button"
-          >
+          <button className="button-red-primary" onClick={() => navigate(`/insurance-inicio`)} type="button">
             Conoce más
           </button>
         </article>
