168205c29b109e967209a81ee83ca279e375c835
JoshDeveloper25 <amandasarvelasquez@gmail.com>
2024-05-11 15:46:28 -0600

    Pagina Publicaciones de casa de valores terminada

--- src/pages/LayoutEmpresas/AtlantidaCasaValores/AtlantidaCasaValoresPublicaciones/AtlantidaCasaValoresPublicaciones.jsx
+++ src/pages/LayoutEmpresas/AtlantidaCasaValores/AtlantidaCasaValoresPublicaciones/AtlantidaCasaValoresPublicaciones.jsx
@@ -1,8 +1,10 @@
-import FooterCasaValores from "../../../../components/FooterCasaValores";
-import NavbarCasaValores from "../../../../components/NavbarCasaValores";
-import SubFooter from "../../../../components/SubFooter";
 import AtlantidaCasaValoresHeader from "./components/AtlantidaCasaValoresHeader";
 import AtlantidaCasaValoresPosts from "./components/AtlantidaCasaValoresPosts";
+// import FooterCasaValores from "../../../../components/FooterCasaValores";
+import NavbarCasaValores from "../../../../components/NavbarCasaValores";
+import SubFooter from "../../../../components/SubFooter";
+import Footer from "../../../../components/Footer";
+import RedBanner from "../../../../components/RedBanner";
 
 const AtlantidaCasaValoresPublicaciones = () => {
   return (
@@ -13,19 +15,23 @@ const AtlantidaCasaValoresPublicaciones = () => {
       {/* Header Casa de Valores */}
       <AtlantidaCasaValoresHeader />
 
+      {/* Banner Rojo Casa de Valores */}
+      <RedBanner />
+
       {/* Posts o publicaciones de Casa de Valores */}
       <AtlantidaCasaValoresPosts />
 
       {/* Subfooter 27 */}
       <SubFooter
-        bgColor={`red`}
+        bgColor={`black`}
         img={`https://dummyimage.com/600x400/000/fff`}
         title={`Juntos hacia el éxito`}
         addClassName={`bg-center`}
       />
 
       {/* Footer Casa de Valores */}
-      <FooterCasaValores />
+      {/* <FooterCasaValores /> */}
+      <Footer />
     </>
   );
 };
--- src/pages/LayoutEmpresas/AtlantidaCasaValores/AtlantidaCasaValoresPublicaciones/components/AtlantidaCasaValoresHeader.jsx
+++ src/pages/LayoutEmpresas/AtlantidaCasaValores/AtlantidaCasaValoresPublicaciones/components/AtlantidaCasaValoresHeader.jsx
@@ -1,16 +1,24 @@
+import headerCasaValores from "../../../../../images/casa-valores-publicaciones-header.png";
+
 const AtlantidaCasaValoresHeader = () => {
   return (
-    <section className="container-page">
-      <div className="flex flex-col lg:flex-row justify-between items-center md:gap-6 gap-0 md:max-w-none max-w-lg mx-auto">
-        <article className="flex-[30%] lg:w-full w-auto text-secondary-color lg:py-8 py-3">
-          <h1 className="text-secondary-color sm:text-[2.6rem] text-[1.8rem] leading-[1.3] font-bold family-nunito-semibold">
-            Publicaciones de Atlántida{" "}
-            <span className="md:block inline">Casa de Valores</span>
-          </h1>
-          <div className="h-1 w-full bg-primary-color my-10"></div>
-        </article>
+    <section className="bg-tertiary-color">
+      <div className="container-page">
+        <div className="flex flex-col lg:flex-row justify-between items-center md:gap-6 gap-0 md:max-w-none max-w-lg mx-auto">
+          <article className="flex-1 lg:w-full w-auto text-secondary-color lg:py-8 py-3">
+            <h1 className="text-secondary-color sm:text-[2.6rem] text-[1.8rem] leading-[1.3] font-bold family-nunito-semibold">
+              <span className="family-nunito-black">Publicaciones</span>{" "}
+              <span className="md:block inline">de Atlántida</span> Casa de
+              Valores
+            </h1>
+          </article>
 
-        <article className="flex-1 text-center"></article>
+          <article className="flex-1 text-center">
+            <div className="max-w-5xl mx-auto">
+              <img className="w-full" src={headerCasaValores} />
+            </div>
+          </article>
+        </div>
       </div>
     </section>
   );
